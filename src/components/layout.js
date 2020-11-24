ec2f4c4ff83b8af100a6c97059e6bd6deb98bc13
John Tigue <john@tigue.com>
2020-11-23 23:08:08 -0800

    Adds Roboto as global font

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./layout.css";
+import "fontsource-roboto"; // Defaults to weight 400 with all styles included.
 
 export default function Layout({ children }) {
   return (
     <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
-      <img src='/reconstrue_combo_mark.svg' width='100%' />
+      <img src='/reconstrue_combo_mark.svg' width='100%' alt='Reconstrue combination brandmark'/>
       <div>
         {children}
       </div>
